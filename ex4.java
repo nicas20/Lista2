b7eac3f2a596d1f972f2316ec3563f463c4b3db6
nicas20 <100000527+nicas20@users.noreply.github.com>
2022-08-08 18:04:15 -0300

    Update ex4.java

--- ex4.java
+++ ex4.java
@@ -4,13 +4,13 @@ import java.util.Scanner;
 
 public class ex4  {
 	public static void main(String[] args) {
-		Scanner ler = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
         float a,b;
         double c;
         System.out.println("Digite o valor a");
-        a = ler.nextFloat();
+        a = sc.nextFloat();
         System.out.println("Digite o valor b");
-        b = ler.nextFloat();
+        b = sc.nextFloat();
         if (a == b) {
         	c = a+b;
         } else {
