abc2a9c5fbbb195aa300abf0af2b867ef354cdef
nicas20 <100000527+nicas20@users.noreply.github.com>
2022-08-08 17:35:35 -0300

    ex6

---
+++ ex6.java
@@ -0,0 +1,42 @@
+package com.mycompany.lista2;
+
+import java.util.Scanner;
+
+public class ex6 {
+
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        float n1, n2, n3;
+        System.out.println("Insira um valor");
+        n1 = ler.nextFloat();
+        System.out.println("Insira um segundo valor");
+        n2 = ler.nextFloat();
+        System.out.println("Insira um terceiro valor");
+        n3 = ler.nextFloat();
+        if (n1 < n2 && n1 < n3) {
+            System.out.print(n1);
+            System.out.print(", ");
+            if (n2 < n3) {
+                System.out.print(n2 + ", " + n3);
+            } else {
+                System.out.print(n3 + ", " + n2);
+            }
+        } else if (n2 < n1 && n2 < n3) {
+            System.out.print(n2);
+            System.out.print(", ");
+            if (n1 < n3) {
+                System.out.print(n1 + ", " + n3);
+            } else {
+                System.out.print(n3 + ", " + n1);
+            }
+        } else if (n3 < n2 && n3 < n1) {
+            System.out.print(n3);
+            System.out.print(", ");
+            if (n1 < n2) {
+                System.out.print(n1 + ", " + n2);
+            } else {
+                System.out.print(n2 + ", " + n1);
+            }
+        }
+    }
+}
