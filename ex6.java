42676ff7952999982ffab9049bf7da5aa6cf776d
nicas20 <100000527+nicas20@users.noreply.github.com>
2022-08-08 18:04:45 -0300

    Update ex6.java

--- ex6.java
+++ ex6.java
@@ -5,14 +5,14 @@ import java.util.Scanner;
 public class ex6 {
 
     public static void main(String[] args) {
-        Scanner ler = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         float n1, n2, n3;
         System.out.println("Insira um valor");
-        n1 = ler.nextFloat();
+        n1 = sc.nextFloat();
         System.out.println("Insira um segundo valor");
-        n2 = ler.nextFloat();
+        n2 = sc.nextFloat();
         System.out.println("Insira um terceiro valor");
-        n3 = ler.nextFloat();
+        n3 = sc.nextFloat();
         if (n1 < n2 && n1 < n3) {
             System.out.print(n1);
             System.out.print(", ");
